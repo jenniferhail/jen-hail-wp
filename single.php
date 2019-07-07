93c96713533878b65a53a638cdfdd3b89688ecc1
Chris Miller <miller.t.chris@gmail.com>
2019-07-07 17:26:13 -0400

    Fixed issue that caused an errors on single.php pages by adding the loop.

--- single.php
+++ single.php
@@ -2,12 +2,14 @@
 
 <section class="layout basic-content">
     <div class="wrapper">
-        <article id="post-<?php the_ID(); ?>" <?php post_class('wrapper'); ?>>
-			<h1 class="title"><?php the_title(); ?></h1>
-			<div class="the-content">
-				<?php the_content(); ?>
-			</div>
-        </article>
+        <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class('wrapper'); ?>>
+                <h1 class="title"><?php the_title(); ?></h1>
+                <div class="the-content">
+                    <?php the_content(); ?>
+                </div>
+            </article>
+        <?php endwhile; endif; ?>
     </div>
 </section>
 
