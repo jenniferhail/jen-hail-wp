5e3904e05904659b76836935636d6e119b4e9f49
Jennifer Hail <jen@mightily.com>
2021-01-31 14:11:40 -0500

    moving index markup to main page template

---
+++ templates/main.php
@@ -0,0 +1,369 @@
+<?php
+/* Template Name: Main */
+get_header();
+?>
+
+    <section id="gallery" class="layout gallery" style="--vh: 1vh;">
+		<div class="wrapper">
+			<div class="row">
+				<img class="img-1" src="<?php echo get_template_directory_uri(); ?>/dist/assets/img/smile.jpg" alt="">
+				<img class="img-2" src="<?php echo get_template_directory_uri(); ?>/dist/assets/img/light-2.jpg" alt="">
+				<img class="img-3" src="<?php echo get_template_directory_uri(); ?>/dist/assets/img/my-desk.jpg" alt="">
+				<img class="img-4" src="<?php echo get_template_directory_uri(); ?>/dist/assets/img/olive.jpg" alt="">
+				<img class="img-5" src="<?php echo get_template_directory_uri(); ?>/dist/assets/img/sand.jpg" alt="">
+				<div class="caption">
+					<span class="counter">1 of 5</span>
+					<p>Photo Credit – <a href="https://www.instagram.com/caterobesonpickens/" target="_blank">Cate Robeson-Pickens</a></p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="about" class="layout about">
+		<div class="wrapper">
+			<div class="row">
+				<div class="col">
+					
+				</div>
+				<div class="col">
+					<h2>About Me</h2>
+					<h3>Web Developer + Designer</h3>
+					<p>Hey, my name is Jennifer, but you can call me Jen.
+							I'm a web developer and graphic designer, currently
+							wrangling code at <a href="https://mightily.com" target="_blank">Mightily</a>. I'm a big fan of hand crafted coffee — I make a
+							Chemex nearly every morning — and I'm inspired by
+							minimalism in architecture, interior design, and
+							photography.</p>					
+				</div>
+				<div class="col">
+				</div>
+				<div class="col">
+					
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="work" class="layout table">
+		<div class="wrapper">
+			<div class="intro row">
+				<div class="col">
+					<h2>Selected Work</h2>
+					<h3>Things I've Made</h3>
+				</div>
+			</div>
+			<div class="grid">
+				<div class="row titles">
+					<div class="col">
+						<span></span>
+					</div>
+					<div class="col">
+						<span>Project</span>
+					</div>
+					<div class="col notes">
+						<span>Notes</span>
+					</div>
+				</div>
+				<div class="row project">
+					<div class="col first">
+						<span>2020</span> <span class="category">Dev</span>
+					</div>
+					<div class="col">
+						<span>NBA 2 Lou</span>
+						<a class="link" href="https://nba2lou.com/" target="_blank"><span class="visually-hidden">NBA 2 Lou</span></a>
+						<span class="hidden-msg">⟶</span>
+					</div>
+					<div class="col notes">
+						We're bringing the next NBA team to Louisville, KY. Built at Mightily.
+					</div>
+				</div>
+				<div class="row project">
+					<div class="col first">
+						<span>2020</span> <span class="category">Dev</span>
+					</div>
+					<div class="col">
+						<span>Lifequipt</span>
+						<a class="link" href="https://lifequipt.com/" target="_blank"><span class="visually-hidden">Lifequipt</span></a>
+						<span class="hidden-msg">⟶</span>
+					</div>
+					<div class="col notes">
+						A neat little landing page for an app company that helps companies improve the health and wellbeing of their employees. Built at Mightily.
+					</div>
+				</div>
+				<div class="row project">
+					<div class="col first">
+						<span>2019</span> <span class="category">Dev</span>
+					</div>
+					<div class="col">
+						<span>Hosparus Health</span>
+						<a class="link" href="https://hosparushealth.org/" target="_blank"><span class="visually-hidden">Hosparus Health</span></a>
+						<span class="hidden-msg">⟶</span>
+					</div>
+					<div class="col notes">
+						A smooth, approachable website for a difficult reality. Built at Mightily.
+					</div>
+				</div>
+				<div class="row project">
+					<div class="col first">
+						<span>2019</span> <span class="category">Dev</span>
+					</div>
+					<div class="col">
+						<span>Gravely Brewing</span>
+						<a class="link" href="https://gravelybrewing.com/" target="_blank"><span class="visually-hidden">Gravely Brewing</span></a>
+						<span class="hidden-msg">⟶</span>
+					</div>
+					<div class="col notes">
+						A clever site for a hip, local brewery slinging the best beer, tunes, and food in town. Built at Mightily.
+					</div>
+				</div>
+				<div class="row project">
+					<div class="col first">
+						<span>2018</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Love, Leon</span>
+						<!-- <a class="link" href="https://" target="_blank"><span class="visually-hidden">Love, Leon</span></a> -->
+						<!-- <span class="hidden-msg">⟶</span> -->
+					</div>
+					<div class="col notes">
+						Rebranding a husband-and-wife photography team that celebrates intimacy, adventure, and wild love.
+					</div>
+				</div>
+				<div class="row project">
+					<div class="col first">
+						<span>2018</span> <span class="category">Dev,</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Forage</span>
+						<a class="link" href="https://forageplants.com/" target="_blank"><span class="visually-hidden">Forage</span></a>
+						<span class="hidden-msg">⟶</span>
+					</div>
+					<div class="col notes">
+						A cool brand and website for a growing plant shop.
+					</div>
+				</div>
+				<div class="row project">
+					<div class="col first">
+						<span>2018</span> <span class="category">Dev</span>
+					</div>
+					<div class="col">
+						<span>Investor's Heritage</span>
+						<a class="link" href="https://investorsheritage.com/" target="_blank"><span class="visually-hidden">Investor's Heritage</span></a>
+						<span class="hidden-msg">⟶</span>
+					</div>
+					<div class="col notes">
+						A classy, professional site that elevated an insurance company. Built at Mightily.
+					</div>
+				</div>
+				<div class="row project">
+					<div class="col first">
+						<span>2018</span> <span class="category">Dev</span>
+					</div>
+					<div class="col">
+						<span>Metro United Way</span>
+						<a class="link" href="https://metrounitedway.org/" target="_blank"><span class="visually-hidden">Metro United Way</span></a>
+						<span class="hidden-msg">⟶</span>
+					</div>
+					<div class="col notes">
+						A website to unite a community — featuring full-bleed images and eye-catching headlines. Built at Mightily.
+					</div>
+				</div>
+				<!-- <div class="row project">
+					<div class="col first">
+						<span>2017</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Natalie O Design</span>
+					</div>
+					<div class="col notes">
+						Maecenas sed diam eget risus varius blandit sit amet non magna.
+					</div>
+				</div> -->
+				<!-- <div class="row project">
+					<div class="col first">
+						<span>2017</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Babe Denny</span>
+					</div>
+					<div class="col notes">
+						Maecenas sed diam eget risus varius blandit sit amet non magna.
+					</div>
+				</div> -->
+				<!-- <div class="row project">
+					<div class="col first">
+						<span>2017</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Whitney Neal Studios</span>
+					</div>
+					<div class="col notes">
+						Maecenas sed diam eget risus varius blandit sit amet non magna.
+					</div>
+				</div> -->
+				<!-- <div class="row project">
+					<div class="col first">
+						<span>2017</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>The Humble Lion</span>
+					</div>
+					<div class="col notes">
+						Maecenas sed diam eget risus varius blandit sit amet non magna.
+					</div>
+				</div> -->
+				<!-- <div class="row project">
+					<div class="col first">
+						<span>2017</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Melissa Sung Photography</span>
+					</div>
+					<div class="col notes">
+						Maecenas sed diam eget risus varius blandit sit amet non magna.
+					</div>
+				</div> -->
+				<!-- <div class="row project">
+					<div class="col first">
+						<span>2017</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Chandle Rose Photography</span>
+					</div>
+					<div class="col notes">
+						Maecenas sed diam eget risus varius blandit sit amet non magna.
+					</div>
+				</div> -->
+				<!-- <div class="row project">
+					<div class="col first">
+						<span>2017</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Happy Camper Film & Photo</span>
+					</div>
+					<div class="col notes">
+						Maecenas sed diam eget risus varius blandit sit amet non magna.
+					</div>
+				</div> -->
+				<!-- <div class="row project">
+					<div class="col first">
+						<span>2017</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Level Up</span>
+					</div>
+					<div class="col notes">
+						Maecenas sed diam eget risus varius blandit sit amet non magna.
+					</div>
+				</div> -->
+				<div class="row project">
+					<div class="col first">
+						<span>2017</span> <span class="category">Dev,</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Crowned Events</span>
+						<a class="link" href="http://crowned-events.com/" target="_blank"><span class="visually-hidden">Crowned Events</span></a>
+						<span class="hidden-msg">⟶</span>
+					</div>
+					<div class="col notes">
+						A clean identity and website update to show off the exquisite work of a SoCal wedding planner.
+					</div>
+				</div>
+				<div class="row project">
+					<div class="col first">
+						<span>2016</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Foe & Dear x Faculty</span>
+					</div>
+					<div class="col notes">
+						Museum-inspired package design for a boutique jewelry designer in Vancouver, Canada.
+					</div>
+				</div>
+				<!-- <div class="row project">
+					<div class="col first">
+						<span>2016</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Carley Randall</span>
+					</div>
+					<div class="col notes">
+						Maecenas sed diam eget risus varius blandit sit amet non magna.
+					</div>
+				</div> -->
+				<!-- <div class="row project">
+					<div class="col first">
+						<span>2016</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Alyssa Joy Photography</span>
+					</div>
+					<div class="col notes">
+						Maecenas sed diam eget risus varius blandit sit amet non magna.
+					</div>
+				</div> -->
+				<div class="row project">
+					<div class="col first">
+						<span>2016</span> <span class="category">Design</span>
+					</div>
+					<div class="col">
+						<span>Story Louisville</span>
+						<!-- <a class="link" href="http://" target="_blank"><span class="visually-hidden">Story Louisville</span></a>
+						<span class="hidden-msg">⟶</span> -->
+					</div>
+					<div class="col notes">
+						Logo design for an empowering, female-led organization focused on encouraging entrepreneurship and building community.
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="notes" class="layout notes">
+		<div class="wrapper">
+			<div class="intro row">
+				<div class="col">
+					<h2>Notes & Photos</h2>
+					<h3>Things I've Written</h3>
+				</div>
+			</div>
+			<?php
+				$args = array(
+					'post_type' => 'post',
+					'posts_per_page' => 10,
+					'orderby' => 'date',
+					'order' => 'DESC' // newest first
+				);
+				$query = new WP_Query( $args );
+			?>
+
+			<?php if($query->have_posts()) : ?>
+				<div class="row">
+					<ul class="note-list">
+						<?php while($query->have_posts()) : $query->the_post(); ?>
+							<li class="item">
+								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+								<?php foreach (get_the_terms(get_the_ID(), 'category') as $cat): ?>
+									<span class="category"><?php echo $cat->name; ?></span>
+								<?php endforeach; ?>
+							</li>
+						<?php endwhile; ?>
+					</ul>
+				</div>
+			<?php endif; ?>
+			
+		</div>
+	</section>
+
+	<?php if(acf_activated() && have_rows('layouts')) : ?>
+
+		<?php while (acf_activated() && have_rows('layouts')) : the_row(); ?>
+
+			<?php $layout_type = get_row_layout(); ?>
+			<?php include(locate_template('layouts/layout-' . $layout_type . '.php')); ?>
+
+		<?php endwhile; ?>
+
+	<?php endif; ?>
+
+<?php get_footer(); ?>
